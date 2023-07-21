2d9489e9035cb497e2c5ad62e77110378876beee
Herve Nkurikiyimfura <nkuliherve@gmail.com>
2023-07-21 08:30:07 +0200

    test routes for static site deployment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from "react";
-import { UploadComponent } from "./Components/uploadComponent";
-import "./Components/index.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
-export default function App() {
-  return <UploadComponent />;
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
 
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { UploadComponent } from "../Components/uploadComponent";
+import "../Components/index.css";
+
+const Home = () => {
+  return (
+    <UploadComponent />
+  );
+};
+
+export default Home;
